3acd932834bfeb5341233b7fa27087efe76e3348
jobs <2821740092@qq.com>
2020-07-01 17:08:05 +0800

    feat: 修改文件结构

--- src/store/reducer.tsx
+++ src/store/reducer.tsx
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import { reducer as headReducer } from "../components/Head/store/index";
-import { reducer as loginSignReducer } from "../components/LoginSign/store/index";
+import { reducer as headReducer } from "../page/Head/store/index";
+import { reducer as loginSignReducer } from "../page/LoginSign/store/index";
 
 export default combineReducers({
   head: headReducer,
